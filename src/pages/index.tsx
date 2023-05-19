8428057bbd111fae70992e48c7be618a9c25c583
s1f10230014 <s1f102300144@iniad.org>
2023-05-20 07:51:22 +0900

    board数字設置関数化

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -96,28 +96,72 @@ const Home = () => {
     }
   }
 
-  //board数字設置
-  for (const one_left_click_position of left_click_positions) {
-    if (bombMap[one_left_click_position[0]][one_left_click_position[1]] !== 1) {
-      let bomb_count = 0;
+  // //空白連鎖
+  // const empty_cell_chain = (cell_list: number[][]) => {
+  //   for (const empty_cell_posicion of cell_list) {
+  //     const empty_cell_list: number[][] = [];
+  //     const bomb_count = 0;
 
-      for (const next_cell_course of directions) {
-        if (
-          bombMap[one_left_click_position[0] + next_cell_course[0]] !== undefined &&
-          bombMap[one_left_click_position[1] + next_cell_course[1]] !== undefined &&
-          bombMap[one_left_click_position[0] + next_cell_course[0]][
-            one_left_click_position[1] + next_cell_course[1]
-          ] === 1
-        ) {
-          bomb_count++;
+  //     if (bombMap[empty_cell_posicion[0]][empty_cell_posicion[1]] !== 1) {
+  //       for (const next_cell_course of directions) {
+  //         if (
+  //           bombMap[empty_cell_posicion[0] + next_cell_course[0]] !== undefined &&
+  //           bombMap[empty_cell_posicion[1] + next_cell_course[1]] !== undefined &&
+  //           bombMap[empty_cell_posicion[0] + next_cell_course[0]][
+  //             empty_cell_posicion[1] + next_cell_course[1]
+  //           ] === 0
+  //         ) {
+  //         }
+  //       }
+  //     }
+  //     board[empty_cell_posicion[0]][empty_cell_posicion[1]] = bomb_count;
+
+  //     if (bomb_count === 0) {
+  //       empty_cell_list.push(empty_cell_posicion);
+  //     }
+  //   }
+  //   if (empty_cell_list.length !== 0) {
+  //     empty_cell_chain(empty_cell_list);
+  //   }
+  // };
+
+  //board数字設置関数
+  const bomb_quantity_set = (posicions_list: number[][]) => {
+    const empty_cell_list: number[][] = [];
+
+    for (const one_left_click_position of posicions_list) {
+      if (bombMap[one_left_click_position[0]][one_left_click_position[1]] !== 1) {
+        let bomb_count = 0;
+
+        for (const next_cell_course of directions) {
+          if (
+            bombMap[one_left_click_position[0] + next_cell_course[0]] !== undefined &&
+            bombMap[one_left_click_position[1] + next_cell_course[1]] !== undefined &&
+            bombMap[one_left_click_position[0] + next_cell_course[0]][
+              one_left_click_position[1] + next_cell_course[1]
+            ] === 1
+          ) {
+            bomb_count++;
+          }
+        }
+
+        board[one_left_click_position[0]][one_left_click_position[1]] = bomb_count;
+
+        if (bomb_count === 0) {
+          empty_cell_list.push(one_left_click_position);
         }
       }
-
-      board[one_left_click_position[0]][one_left_click_position[1]] = bomb_count;
     }
-  }
 
-  console.log('left_click_positions', left_click_positions);
+    // if (empty_cell_list.length !== 0) {
+    //   empty_cell_chain(empty_cell_list);
+    // }
+    // console.log('空ますリスト', empty_cell_list);
+
+    console.log('left_click_positions', left_click_positions);
+  };
+
+  bomb_quantity_set(left_click_positions);
 
   const clikstone = (x: number, y: number) => {
     console.log('クリック※xy順', x, y);
